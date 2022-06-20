b40517afeecf208f7afa9e8c6bbf4bef08d2b1ce
Lurgas <dbunk0214@gmail.com>
2022-06-20 08:26:25 +0200

    commit

--- src/components/AsciiColorValue.tsx
+++ src/components/AsciiColorValue.tsx
@@ -3,7 +3,7 @@ import CSS from 'csstype';
 import { col } from "./EncodedAreaComponent";
 
 const AsciiColorValue = (props: col) => {
-    const lvl = ("00" + props.value).substr(-2, 2);
+    const lvl = ("00" + props.value).slice(-2);
 
     const clrCode = "#" + "c8" + lvl + "eb";
 
